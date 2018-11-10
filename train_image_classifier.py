22c9df4fc2839e387b199086b9d8f5f6ef755428
zhouzhiqi <zhouzhiqi1234@outlook.com>
2018-11-10 12:14:27 +0800

    666

--- train_image_classifier.py
+++ train_image_classifier.py
@@ -1,16 +1,20 @@
-# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+# Copyright(n.  版权; 著作权) 2016 The TensorFlow Authors. 
+# All Rights Reserved(adj.  留作专用的; 储备的; 预订的; 沉默寡言的).
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
+# Licensed(adj.  得到许可的) under the Apache License, Version 2.0 (the "License");
+# you may not use this file except(v.  除, 反对, 除外; 反对; 表示异议) in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Unless required(adj.  必须的; 必修的) by applicable(adj.  可应用的; 可适用的) law or agreed to in writing, 
+# software distributed(v.  分发; 散布; 分配) under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES(n.  担保, 根据, 保证) OR CONDITIONS(n.  情况; 环境; 状态; 形势) OF ANY KIND, 
+# either express(n.  快车; 专使; 快递) or implied(v.  暗示; 意味).
+# See the License for the specific(n.  特性; 详情; 特效药; 详细说明书) language 
+# governing(v.  统治; 决定, 影响, 指导; 管理; 控制; 进行统治, 管理, 执政; 居支配地位) 
+# permissions(n.  许可, 允许, 同意; 施予; 给使用者权力进入某资料源 (计算机用语)) and
+# limitations(n.  限制, 限制因素; 极限, 限度; 局限; 追诉时效) under the License.
 # ==============================================================================
 """Generic training script that trains a model using a given dataset."""
 
